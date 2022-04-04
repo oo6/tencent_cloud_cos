7e2689e556f0fded0deafa88c08e66898cac77e1
Milo Lee <leexu.dev@gmail.com>
2022-04-05 02:11:06 +0800

    feat: list objects and objects with versions

--- lib/cos/bucket.ex
+++ lib/cos/bucket.ex
@@ -36,8 +36,7 @@ defmodule COS.Bucket do
             headers: keyword(),
             tesla_opts: Tesla.Env.opts()
           ]
-        ) ::
-          Tesla.Env.t()
+        ) :: Tesla.Env.t()
   def put(host, opts \\ []) do
     body = if opts[:body], do: {:create_bucket_configuration, opts[:body]}
     headers = opts[:headers] || []
@@ -49,5 +48,145 @@ defmodule COS.Bucket do
       headers: headers,
       opts: opts[:tesla_opts]
     )
+  end
+
+  @doc """
+  删除存储桶 - [腾讯云文档](https://cloud.tencent.com/document/product/436/7732)
+  """
+  @spec delete(host :: binary(), opts :: [tesla_opts: Tesla.Env.opts()]) :: Tesla.Env.t()
+  def delete(host, opts \\ []) do
+    HTTPClient.request(method: :delete, url: host, opts: opts[:tesla_opts])
+  end
+
+  @doc """
+  检索存储桶 - [腾讯云文档](https://cloud.tencent.com/document/product/436/7735)
+
+  确认该存储桶是否存在，是否有权限访问。有以下几种情况：
+    - 存储桶存在且有读取权限，返回 HTTP 状态码为200。
+    - 无存储桶读取权限，返回 HTTP 状态码为403。
+    - 存储桶不存在，返回 HTTP 状态码为404。
+  """
+  @spec head(host :: binary(), opts :: [tesla_opts: Tesla.Env.opts()]) :: Tesla.Env.t()
+  def head(host, opts \\ []) do
+    HTTPClient.request(method: :head, url: host, opts: opts[:tesla_opts])
+  end
+
+  @doc """
+  查询对象列表 - [腾讯云文档](https://cloud.tencent.com/document/product/436/7734)
+  """
+  @spec list_objects(
+          host :: binary(),
+          opts :: [
+            query: %{
+              optional(:prefix) => binary(),
+              optional(:delimiter) => binary(),
+              optional(:marker) => binary(),
+              optional(:encoding_type) => binary(),
+              optional(:max_keys) => pos_integer()
+            },
+            tesla_opts: Tesla.Env.opts()
+          ]
+        ) :: Tesla.Env.t()
+  def list_objects(host, opts \\ []) do
+    query =
+      case opts[:query] do
+        nil ->
+          %{}
+
+        query ->
+          {query, other_query} = Map.split(query, [:prefix, :delimiter, :marker])
+
+          query
+          |> Map.merge(%{
+            "encoding-type": other_query[:encoding_type],
+            "max-keys": other_query[:max_keys]
+          })
+          |> Map.filter(&elem(&1, 1))
+      end
+
+    with {:ok, response} =
+           HTTPClient.request(
+             method: :get,
+             url: host <> "/",
+             query: query,
+             result_key: "list_bucket_result",
+             opts: opts[:tesla_opts]
+           ) do
+      body =
+        response.body
+        |> Map.update("contents", [], fn contents ->
+          contents
+          |> List.wrap()
+          |> Enum.map(fn content -> Map.update!(content, "size", &String.to_integer/1) end)
+        end)
+        |> Map.update!("is_truncated", &String.to_atom/1)
+        |> Map.update!("max_keys", &String.to_integer/1)
+
+      {:ok, %{response | body: body}}
+    end
+  end
+
+  @doc """
+  查询对象历史版本列表 - [腾讯云文档](https://cloud.tencent.com/document/product/436/35521)
+  """
+  @spec list_objects_with_versions(
+          host :: binary(),
+          opts :: [
+            query: %{
+              optional(:prefix) => binary(),
+              optional(:delimiter) => binary(),
+              optional(:encoding_type) => binary(),
+              optional(:max_keys) => pos_integer(),
+              optional(:key_maker) => binary(),
+              optional(:version_id_marker) => binary()
+            },
+            tesla_opts: Tesla.Env.opts()
+          ]
+        ) :: Tesla.Env.t()
+  def list_objects_with_versions(host, opts \\ []) do
+    case opts[:query] do
+      nil ->
+        %{}
+
+      query ->
+        {other_query, query} = Map.split(query, [:prefix, :delimiter])
+
+        query
+        |> Map.merge(%{
+          "encoding-type": other_query[:encoding_type],
+          "max-keys": other_query[:max_keys],
+          "key-marker": other_query[:key_maker],
+          "version-id-marker": other_query[:version_id_marker]
+        })
+        |> Map.filter(&elem(&1, 1))
+    end
+
+    with {:ok, response} <-
+           HTTPClient.request(
+             method: :get,
+             url: host <> "/?versions",
+             result_key: "list_versions_result",
+             opts: opts[:tesla_opts]
+           ) do
+      body =
+        response.body
+        |> Map.update("version", [], fn versions ->
+          versions
+          |> List.wrap()
+          |> Enum.map(fn version ->
+            version
+            |> Map.update!("size", &String.to_integer/1)
+            |> Map.update!("is_latest", &String.to_atom/1)
+            |> Map.update!("version_id", fn
+              "null" -> nil
+              version_id -> version_id
+            end)
+          end)
+        end)
+        |> Map.update!("is_truncated", &String.to_atom/1)
+        |> Map.update!("max_keys", &String.to_integer/1)
+
+      {:ok, %{response | body: body}}
+    end
   end
 end
--- test/cos/bucket_test.exs
+++ test/cos/bucket_test.exs
@@ -21,4 +21,139 @@ defmodule COS.BucketTest do
                body: %{bucket_a_z_config: "MAZ"}
              )
   end
+
+  test "handling list objects return value formats" do
+    mock(fn _ ->
+      text("""
+      <?xml version='1.0' encoding='utf-8' ?>
+      <ListBucketResult>
+        <Contents>
+          <Key>foo/</Key>
+          <LastModified>2022-04-04T11:40:35.000Z</LastModified>
+          <ETag>&quot;d41d8cd98f00b204e9800998ecf8427e&quot;</ETag>
+          <Size>0</Size>
+          <Owner>
+            <ID>blah</ID>
+            <DisplayName>blah</DisplayName>
+          </Owner>
+          <StorageClass>STANDARD</StorageClass>
+        </Contents>
+        <Contents>
+          <Key>foo/bar.txt</Key>
+          <LastModified>2022-04-04T11:40:48.000Z</LastModified>
+          <ETag>&quot;d41d8cd98f00b204e9800998ecf8427e&quot;</ETag>
+          <Size>1024</Size>
+          <Owner>
+          <ID>blah</ID>
+          <DisplayName>blah</DisplayName>
+          </Owner>
+          <StorageClass>STANDARD</StorageClass>
+        </Contents>
+        <Name>bucket-1250000000</Name>
+        <IsTruncated>fasle</IsTruncated>
+        <MaxKeys>1000</MaxKeys>
+        <Prefix/>
+        <Marker/>
+        <NextMarker/>
+      </ListBucketResult>
+      """)
+
+      assert {:ok,
+              %{
+                body: %{
+                  "contents" => [
+                    %{"key" => "foo/", "size" => 0},
+                    %{"key" => "foo/bar.txt", "size" => 1024}
+                  ],
+                  "is_truncated" => false,
+                  "max_keys" => 1000
+                }
+              }} = Bucket.list_objects("https://bucket-1250000000.cos.ap-beijing.myqcloud.com")
+    end)
+  end
+
+  test "handling list objects with versions return value formats" do
+    mock(fn _ ->
+      text("""
+      <ListVersionsResult>
+        <Version>
+          <Key>foo</Key>
+          <VersionId>MTg0NDUwOTQ5ODUyOTk0MjEwNzY</VersionId>
+          <IsLatest>true</IsLatest>
+          <LastModified>2022-04-04T16:06:50.000Z</LastModified>
+          <ETag>&quot;310dcbbf4cce62f762a2aaa148d556bd&quot;</ETag>
+          <Size>2</Size>
+          <StorageClass>STANDARD</StorageClass>
+          <Owner>
+            <ID>blah</ID>
+            <DisplayName>blah</DisplayName>
+          </Owner>
+        </Version>
+        <Version>
+          <Key>foo</Key>
+          <VersionId>null</VersionId>
+          <IsLatest>false</IsLatest>
+          <LastModified>2022-04-04T15:59:17.000Z</LastModified>
+          <ETag>&quot;c4ca4238a0b923820dcc509a6f75849b&quot;</ETag>
+          <Size>1</Size>
+          <StorageClass>STANDARD</StorageClass>
+          <Owner>
+            <ID>blah</ID>
+            <DisplayName>blah</DisplayName>
+          </Owner>
+        </Version>
+        <Version>
+          <Key>bar</Key>
+          <VersionId>MTg0NDUwOTQ5ODUyODM1NDE1NDc</VersionId>
+          <IsLatest>true</IsLatest>
+          <LastModified>2022-04-04T16:07:06.000Z</LastModified>x
+          <ETag>&quot;c4ca4238a0b923820dcc509a6f75849b&quot;</ETag>
+          <Size>1</Size>
+          <StorageClass>STANDARD</StorageClass>
+          <Owner>
+            <ID>blah</ID>
+            <DisplayName>blah</DisplayName>
+          </Owner>
+        </Version>
+        <Name>bucket-1250000000</Name>
+        <IsTruncated>false</IsTruncated>
+        <MaxKeys>1000</MaxKeys>
+        <Prefix/>
+        <KeyMarker/>
+        <VersionIdMarker/>
+      </ListVersionsResult>
+      """)
+
+      assert {:ok,
+              %{
+                body: %{
+                  "version" => [
+                    %{
+                      "key" => "foo",
+                      "is_latest" => true,
+                      "size" => 2,
+                      "version_id" => "MTg0NDUwOTQ5ODUyOTk0MjEwNzY"
+                    },
+                    %{
+                      "key" => "foo",
+                      "is_latest" => false,
+                      "size" => 1,
+                      "version_id" => nil
+                    },
+                    %{
+                      "key" => "br",
+                      "is_latest" => true,
+                      "size" => 1,
+                      "version_id" => "MTg0NDUwOTQ5ODUyODM1NDE1NDc"
+                    }
+                  ],
+                  "is_truncated" => false,
+                  "max_keys" => 1000
+                }
+              }} =
+               Bucket.list_objects_with_versions(
+                 "https://bucket-1250000000.cos.ap-beijing.myqcloud.com"
+               )
+    end)
+  end
 end
